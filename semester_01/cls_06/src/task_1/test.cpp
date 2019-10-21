9f79f1c70db18601610ed0251c1db6c68a835f4e
Roman Golyshev <anstreth@gmail.com>
2019-10-21 20:15:10 +0300

    Semester 1, Classwork 06: Add task 1 and 2

---
+++ semester_01/cls_06/src/task_1/test.cpp
@@ -0,0 +1,53 @@
+#include "shared_buffer.hpp"
+
+#include <cassert>
+
+static void test_shared_buffer() {
+    {
+        static_assert(!std::is_convertible_v<size_t, shared_buffer>);
+
+        shared_buffer buffer { 10 };
+        shared_buffer const ref = buffer;
+
+        assert(buffer.get_size() == 10);
+        assert(ref.get_size() == 10);
+
+        for (size_t i = 0; i < buffer.get_size(); ++i) {
+            assert(buffer.get_data()[i] == 0);
+            assert(ref.get_data()[i] == 0);
+
+            buffer.get_data()[i] = char(i);
+            assert(ref.get_data()[i] == char(i));
+        }
+    }
+
+    {
+        static_assert(std::is_copy_constructible_v<shared_buffer>);
+        static_assert(std::is_copy_assignable_v<shared_buffer>);
+
+        shared_buffer buf1 {100};
+        shared_buffer buf2 {buf1};
+        shared_buffer buf3 { 10 };
+
+        shared_buffer const& ref = buf1;
+
+        assert(buf1.get_size() == buf2.get_size() && "Buffers should have the same size");
+        assert(buf1.get_data() == buf2.get_data() && "Buffers should point to the same memory");
+        assert(std::strcmp(buf1.get_data(), buf2.get_data()) == 0 && "Buffers should have equal content")
+
+        buf3 = ref;
+        assert(buf1.get_data() == buf3.get_data() && "After assignment, buf1 and buf3 should point to the same memory too");
+    }
+
+    {
+        for (size_t i = 0; i < 100; ++i) {
+            shared_buffer b { i };
+            shared_buffer const& ref = b;
+            b = ref;
+        }
+    }
+}
+
+int main() {
+    test_shared_buffer();
+}
---
+++ semester_01/cls_06/src/task_2/test.cpp
@@ -0,0 +1,51 @@
+#include "lazy_string.hpp"
+
+#include <cassert>
+#include <cstring>
+#include <string>
+
+static void test_lazy_string() {
+    {
+        lazy_string s1;
+        lazy_string const& ref = s1;
+
+        assert(s1.get_size() == 0);
+        assert(ref.get_size() == 0);
+        assert(s1.c_str()[0] == '\0');
+        assert(ref.c_str()[0] == '\0');
+    }
+
+    {
+        lazy_string s1 = "Hello world";
+        lazy_string const s2 = s1;
+
+        assert(s2.get_size() == s1.get_size());
+        assert(s1.c_str() == s2.c_str());
+
+        assert(s1.get_size() == sizeof("Hello world") - 1);
+        assert(std::strcmp(s1.c_str(), "Hello world") == 0);
+    }
+
+    {
+        lazy_string s1;
+        for (size_t i = 0; i < 10; ++i) {
+            s1 = concat(s1, "A");
+        }
+
+        assert(s1.get_size() == 10);
+        assert(std::strcmp(s1.c_str(), "AAAAAAAAAA") == 0);
+    }
+
+    {
+        std::string str {"Hello, world!"};
+        lazy_string lazy_str {str.c_str() };
+
+        for (size_t i = 0; i < str.size(); ++i) {
+            assert(str[i] == lazy_str.at(i));
+        }
+    }
+}
+
+int main() {
+    test_lazy_string();
+}
