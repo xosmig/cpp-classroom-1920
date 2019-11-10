fa1dea864d264f2c222e8714f855e963432ee2f6
Roman Golyshev <anstreth@gmail.com>
2019-11-11 00:20:29 +0300

    Semester 1, Classwork 08: Add task 1 test

---
+++ semester_01/cls_08/src/task_1/test.cpp
@@ -0,0 +1,53 @@
+#include "my_ostream.h"
+#include "my_ostream_con.h"
+
+#include <cassert>
+#include <iostream>
+#include <sstream>
+#include <functional>
+
+static void test_with_replaced_cout(std::function<void(std::ostringstream const& sout)> action) {
+    std::ostringstream buffer;
+	std::streambuf* coutbuf = std::cout.rdbuf();
+	std::cout.rdbuf(buffer.rdbuf());
+
+    action(buffer);
+
+	std::cout.rdbuf(coutbuf);
+}
+
+static void test_console_stream() {
+    test_with_replaced_cout([](auto const& sout) {
+        cls_08::my_ostream_con stream;
+        cls_08::my_ostream& ref = stream;
+
+        ref << "Hello!";
+
+        assert(sout.str() == "Hello!");
+    });
+
+    test_with_replaced_cout([](auto const& sout) {
+        cls_08::my_ostream_con stream;
+        cls_08::my_ostream& ref = stream;
+
+        ref << int(10) << " " << int(-10) << " " << int(1024);
+
+        assert(sout.str() == "10 -10 1024");
+    });
+
+
+    test_with_replaced_cout([](auto const& sout) {
+        cls_08::my_ostream_con stream;
+        cls_08::my_ostream& ref = stream;
+
+        ref << double(10.0) << " " << double(-10.1) << " " << double(1024);
+
+        assert(sout.str() == "10 -10.1 1024");
+    });
+}
+
+int main() {
+    test_console_stream();
+
+    return 0;
+}
