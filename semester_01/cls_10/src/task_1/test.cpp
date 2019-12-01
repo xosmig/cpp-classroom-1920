f149c5858398dc92e5f938f633135e1407b79405
Roman Golyshev <anstreth@gmail.com>
2019-12-01 16:38:01 +0300

    Semester 1, Classwork 10: Add tests for task 1, 2

---
+++ semester_01/cls_10/src/task_1/test.cpp
@@ -0,0 +1,90 @@
+#include "scoped_ptr.hpp"
+
+#include <cassert>
+#include <string>
+#include <type_traits>
+
+static void test_scoped_ptr() {
+    static_assert(
+        !std::is_convertible<int*, cls_10::scoped_ptr<int>>::value,
+        "scoped ptr should not have implicit constructor from pointer!"
+    );
+
+    static_assert(
+        !std::is_copy_constructible<cls_10::scoped_ptr<int>>::value,
+        "scoped ptr should not be copiable"
+    );
+
+    static_assert(
+        !std::is_move_constructible<cls_10::scoped_ptr<int>>::value,
+        "scoped ptr should not be movable"
+    );
+
+    static_assert(
+        !std::is_copy_assignable<cls_10::scoped_ptr<int>>::value,
+        "scoped ptr should not be copiable by operator="
+    );
+
+    static_assert(
+        !std::is_move_assignable<cls_10::scoped_ptr<int>>::value,
+        "scoped ptr should not be movable by operator="
+    );
+
+    static_assert(
+        std::is_same<cls_10::scoped_ptr<int>::element_type, int>::value,
+        "scoped ptr should contain element_type"
+    );
+
+    static_assert(
+        std::is_constructible<bool, cls_10::scoped_ptr<int>>::value // explicit conversion
+        && !std::is_convertible<cls_10::scoped_ptr<int>, bool>::value, // implicit conversion
+        "scoped ptr should convertible to the bool, but not implicitly"
+    );
+
+    {
+        cls_10::scoped_ptr<int> empty_ptr;
+
+        assert(!empty_ptr);
+        assert(empty_ptr.get() == nullptr);
+    }
+
+    {
+        struct entity {
+            int f1;
+            std::string f2;
+        };
+
+        static_assert(
+            std::is_same<cls_10::scoped_ptr<entity>::element_type, entity>::value,
+            "scoped ptr should contain element_type"
+        );
+
+        cls_10::scoped_ptr<entity> const ptr{new entity{10, "hello"}};
+
+        assert(ptr->f1 == 10);
+        assert(ptr->f2 == "hello");
+
+        assert((*ptr).f1 == 10);
+        assert((*ptr).f2 == "hello");
+
+        assert(ptr.get()->f1 == 10);
+        assert(ptr.get()->f2 == "hello");
+    }
+
+    {
+        cls_10::scoped_ptr<int> ptr{new int{10}};
+        assert(*ptr == 10);
+        assert(ptr);
+
+        ptr.reset();
+        assert(ptr.get() == nullptr);
+        assert(!ptr);
+
+        ptr.reset(new int{20});
+        assert(*ptr == 20);
+    }
+}
+
+int main() {
+    test_scoped_ptr();
+}
---
+++ semester_01/cls_10/src/task_2/test.cpp
@@ -0,0 +1,104 @@
+#include "shared_ptr.hpp"
+
+#include <cassert>
+#include <string>
+#include <type_traits>
+
+//#define SHARED_PTR_WITH_INCOMPLETE_TYPES
+
+static void test_shared_ptr() {
+    static_assert(
+        !std::is_convertible<int*, cls_10::shared_ptr<int>>::value,
+        "shared ptr should not have implicit constructor from pointer!"
+    );
+
+    static_assert(
+        std::is_same<cls_10::shared_ptr<int>::element_type, int>::value,
+        "shared ptr should contain element_type"
+    );
+
+    static_assert(
+        std::is_constructible<bool, cls_10::shared_ptr<int>>::value // explicit conversion
+        && !std::is_convertible<cls_10::shared_ptr<int>, bool>::value, // implicit conversion
+        "shared ptr should convertible to the bool, but not implicitly"
+    );
+
+    {
+        cls_10::shared_ptr<int> empty_ptr;
+
+        assert(!empty_ptr);
+        assert(empty_ptr.get() == nullptr);
+    }
+
+    {
+        struct entity {
+            int f1;
+            std::string f2;
+        };
+
+        static_assert(
+            std::is_same<cls_10::shared_ptr<entity>::element_type, entity>::value,
+            "shared ptr should contain element_type"
+        );
+
+        cls_10::shared_ptr<entity> const ptr{new entity{10, "hello"}};
+
+        assert(ptr->f1 == 10);
+        assert(ptr->f2 == "hello");
+
+        assert((*ptr).f1 == 10);
+        assert((*ptr).f2 == "hello");
+
+        assert(ptr.get()->f1 == 10);
+        assert(ptr.get()->f2 == "hello");
+    }
+
+    {
+        cls_10::shared_ptr<int> ptr{new int{10}};
+        assert(*ptr == 10);
+        assert(ptr);
+
+        ptr.reset();
+        assert(ptr.get() == nullptr);
+        assert(!ptr);
+
+        ptr.reset(new int{20});
+        assert(*ptr == 20);
+    }
+
+    {
+        cls_10::shared_ptr<int> ptr{new int{10}};
+
+        ptr = static_cast<cls_10::shared_ptr<int> const&>(ptr);
+
+        {
+            cls_10::shared_ptr<int> other{ptr};
+
+            assert(other.get() == ptr.get());
+            assert(&(*other) == &(*ptr));
+        }
+
+        {
+            cls_10::shared_ptr<int> other;
+
+            other = ptr;
+
+            assert(other.get() == ptr.get());
+            assert(&(*other) == &(*ptr));
+        }
+
+    }
+
+#ifdef SHARED_PTR_WITH_INCOMPLETE_TYPES
+    {
+        struct foo;
+        cls_10::shared_ptr<foo> ptr;
+        (void)ptr;
+    }
+#endif
+
+}
+
+int main() {
+    test_shared_ptr();
+}
